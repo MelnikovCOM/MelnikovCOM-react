cc2b386c0d4725f351db67e89e6e10489cd8d434
Aleksei <melnik_89_1989@mail.ru>
2022-09-26 23:05:13 +0300

    add react commit 3

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+function App() {
+	return (
+		<ChatEngine
+			projectID='4a2a50c7-f1b2-4133-a3cd-aa62dfd324f5'
+			userName='ivan'
+			userSecret='123'
+		/>
+	);
+}
+
+export default App
